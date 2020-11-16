282daec64dd8eef9c5bb79ced638b4b00cad5abf
Sergei Gribanov <ssgribanov@gmail.com>
2020-11-16 19:59:14 +0700

    exception changes

--- CMI/cmi/src/main/java/com/cmi/security/AuthentificationFilter.java
+++ CMI/cmi/src/main/java/com/cmi/security/AuthentificationFilter.java
@@ -26,8 +26,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 
-// import java.security.NoSuchAlgorithmException;
 import org.mindrot.jbcrypt.BCrypt;
+
+import com.cmi.database.DataBaseException;
  
 /**
  * This filter verify the access permissions for a user
@@ -46,83 +47,79 @@ public class AuthentificationFilter implements javax.ws.rs.container.ContainerRe
     public void filter(ContainerRequestContext requestContext) {
         Method method = resourceInfo.getResourceMethod();
         //Access allowed for all
-        if( ! method.isAnnotationPresent(PermitAll.class)) {
-	    //Access denied for all
-	    if(method.isAnnotationPresent(DenyAll.class)) {
-                requestContext.abortWith(Response.status(Response.Status.FORBIDDEN)
-					 .entity("Access blocked for all users !!").build());
-                return;
-            }
+        if(method.isAnnotationPresent(PermitAll.class)) {
+	    return;
+	}
+	//Access denied for all
+	if(method.isAnnotationPresent(DenyAll.class)) {
+	    requestContext.abortWith(Response.status(Response.Status.FORBIDDEN)
+				     .entity("Access blocked for all users !!").build());
+	    return;
+	}
               
-            //Get request headers
-            final MultivaluedMap<String, String> headers = requestContext.getHeaders();
+	//Get request headers
+	final MultivaluedMap<String, String> headers = requestContext.getHeaders();
               
-            //Fetch authorization header
-            final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
+	//Fetch authorization header
+	final List<String> authorization = headers.get(AUTHORIZATION_PROPERTY);
               
-            //If no authorization information present; block access
-            if(authorization == null || authorization.isEmpty()) {
-                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
-					 .entity("You cannot access this resource").build());
-                return;
-            }
+	//If no authorization information present; block access
+	if(authorization == null || authorization.isEmpty()) {
+	    requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+				     .entity("You cannot access this resource").build());
+	    return;
+	}
               
-            //Get encoded username and password
-            final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
+	//Get encoded username and password
+	final String encodedUserPassword = authorization.get(0).replaceFirst(AUTHENTICATION_SCHEME + " ", "");
               
-            //Decode username and password
-            String usernameAndPassword = new String(Base64.getDecoder().decode(encodedUserPassword.getBytes()));;
+	//Decode username and password
+	String usernameAndPassword = new String(Base64.getDecoder().decode(encodedUserPassword.getBytes()));;
   
-            //Split username and password tokens
-            final StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
-            final String username = tokenizer.nextToken();
-            final String password = tokenizer.nextToken();
+	//Split username and password tokens
+	final StringTokenizer tokenizer = new StringTokenizer(usernameAndPassword, ":");
+	final String username = tokenizer.nextToken();
+	final String password = tokenizer.nextToken();
               
-            //Verifying Username and password
-            System.out.println(username);
-            System.out.println(password);
-              
-            //Verify user access
-            if(method.isAnnotationPresent(RolesAllowed.class)) {
-                RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
-                Set<String> rolesSet = new HashSet<String>(Arrays.asList(rolesAnnotation.value()));
-		
-                //Is user valid?
-                if( ! isUserAllowed(username, password, rolesSet)) {
-                    requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
-					     .entity("You cannot access this resource").build());
-                    return;
-                }
-            }
-        }
+	//Verify user access
+	if(!method.isAnnotationPresent(RolesAllowed.class)) {
+	    return;
+	}
+	RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
+	Set<String> rolesSet = new HashSet<String>(Arrays.asList(rolesAnnotation.value()));
+	
+	//Is user valid?
+	try {
+	    if( ! isUserAllowed(username, password, rolesSet)) {
+		requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+					 .entity("You cannot access this resource").build());
+		return;
+	    }
+	} catch (DataBaseException e) {
+	    requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+				     .entity("You cannot access this resource").build());
+	} catch (SQLException e) {
+	    requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+				     .entity("You cannot access this resource").build());
+	    return;
+	}
     }
-    private boolean isUserAllowed(final String username, final String password, final Set<String> rolesSet) {
-	boolean isAllowed = false;
+    private boolean isUserAllowed(final String username, final String password, final Set<String> rolesSet) throws SQLException, DataBaseException {
 	String dbUserRole = null;
 	String dbHash = null;
 	String sql = "SELECT userrole, hash FROM users WHERE username = ?";
-	Connection conn = null;
-	try {
-	    conn = DriverManager.getConnection("jdbc:postgresql://172.16.238.11/cmi", "postgres", "1234");
-	    PreparedStatement pstmt  = conn.prepareStatement(sql);
-	    pstmt.setString(1, username);
-	    ResultSet rs  = pstmt.executeQuery();
-            while (rs.next()) {
-		dbUserRole = rs.getString("userrole");
-		dbHash = rs.getString("hash");
-            }
-	    isAllowed = BCrypt.checkpw(password, dbHash) && rolesSet.contains(dbUserRole);
-	} catch (SQLException e) {
-	    // TO DO ... Make same error codes in cases if username does not exist or password does not exist
-	} finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-		// TO DO ...
-            }
-        }
-	return isAllowed;
+	Connection conn = DriverManager.getConnection("jdbc:postgresql://172.16.238.11/cmi", "postgres", "1234");
+	PreparedStatement pstmt  = conn.prepareStatement(sql);
+	pstmt.setString(1, username);
+	ResultSet rs  = pstmt.executeQuery();
+	while (rs.next()) {
+	    dbUserRole = rs.getString("userrole");
+	    dbHash = rs.getString("hash");
+	}
+	if (dbHash == null || dbUserRole == null) {
+	    throw new DataBaseException("Can not find user / userrole / hash!");
+	}
+	conn.close();
+	return BCrypt.checkpw(password, dbHash) && rolesSet.contains(dbUserRole);
     }
 }
